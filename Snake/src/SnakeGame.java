76fd16ae0b4c13d5438b7b1f4780d39baa7f30f2
ireensagh <84302872+ireensagh@users.noreply.github.com>
2021-10-09 23:15:58 -0700

    .

---
+++ Snake/src/SnakeGame.java
@@ -0,0 +1,11 @@
+
+public class SnakeGame {
+
+	public static void main(String[] args) {
+		  
+		new GameFrame();  //shortcut
+		// GameFrame frame = new GameFrame(); if you don't plan to use 'frame'
+
+	}
+
+}
